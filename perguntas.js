d5cdbaed6b56dc4de5ba37043377d1d72268ff6e
professorastelleana <145031308+professorastelleana@users.noreply.github.com>
2024-11-14 10:29:12 -0300

    correção

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao (
+    'FUTEBOL SULAMERICANO',
+    'Em que ano aconteceu o GreNal das américas na Copa Libertadores?',
+    '2020'
+)
+criaCartao (
+    'FUTEBOL SULAMERICANO',
+    'Quem foi o campeão da Libertadores de 2016?',
+    'Atlético Nacional'
+)
+criaCartao (
+    'FUTEBOL SULAMERICANO',
+    'Qual jogador foi o artilheiro no Brasil em 2023?',
+    'German Cano'
+)
